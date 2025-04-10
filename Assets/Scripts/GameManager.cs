f4ff0f598184baa26fee82b225a78e9bd5c9e079
Darm94 <94damianos@gmail.com>
2025-04-11 01:26:21 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,174 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject card ;
+
+    [SerializeField]
+    Vector3[] cards ;
+
+    [SerializeField]
+    Texture2D[] images ;
+
+    [SerializeField]
+    float startX ;
+
+    [SerializeField]
+    float startY ;
+
+    [SerializeField]
+    float planeZ ;
+
+    [SerializeField]
+    float deltax = 1.1f;
+
+    [SerializeField]
+    float deltay = 1.1f;
+
+    [SerializeField]
+    int columns = 5;
+
+    [SerializeField]
+    int rows = 6;
+
+    [SerializeField]
+    GameObject winUI ;
+
+    [SerializeField]
+    AudioSource matchedPairAudioSource ;
+
+    int pairs ;
+
+    InteractiveCard selectedCard1 ;
+    InteractiveCard selectedCard2 ;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        if (rows * columns != images.Length * 2)
+        {
+            Debug.LogWarning("Number of r*c is not equal to provided cards, quit...");
+            return;
+        }
+
+        pairs = columns * rows / 2; //images.Length  its valid too
+
+        //Random Sort
+        System.Random random = new System.Random();
+        images = images.OrderBy(x => random.Next()).ToArray();
+
+        cards = new Vector3[rows * columns];
+
+        float dx = startX;
+        float dy = startY;
+
+        int counter = 0;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                cards[counter++] = new Vector3(dx, dy, planeZ);
+                dx += deltax;
+            }
+            dx = startX;
+            dy += deltay;
+        }
+
+        cards = cards.OrderBy(x => random.Next()).ToArray();
+        
+        // Start creating (instantiate) cards, setting images etc
+        counter = 0;
+
+        int row = 0;
+
+        foreach (Vector3 pos in cards)
+        {
+            GameObject go = Instantiate(card);
+
+            go.SetActive(true); // WE ACTIVATE THE
+
+            go.transform.position = pos;
+
+            // We set card texture (using shader graph)
+            go.GetComponent<MeshRenderer>().material.SetTexture("_MainTexture", images[row]);
+
+            // this event will be called on this class by the InteractiveCard
+            go.GetComponent<InteractiveCard>().OnClicked += SelectedCard;
+
+            // We set Interactive card cover image name
+            go.GetComponent<InteractiveCard>().imageName = images[row].name;
+
+            counter++;
+
+            if (counter % 2 == 0)
+            {
+                row++;
+            }
+            
+        }
+    }
+
+    private void SelectedCard(InteractiveCard card, bool selected)
+    {
+        //Se non ho selezionato la 1 card e viene selezionata
+        if (selectedCard1 == null && selected)
+        {
+            selectedCard1 = card;
+        }
+        //se ho già selezionato la 1 e l' ho deselezionata
+        else if (selectedCard1 == card && !selected)
+        {
+             selectedCard1.ResetMe();
+            selectedCard1 = null;
+        }
+        //se ho già selezionato la 2 e l'  ho deselezionata
+        else if (selectedCard2 != null && card == selectedCard2 && !selected)
+        {
+             selectedCard2.ResetMe();
+            selectedCard2 = null;
+        }
+        //se non ho già selezionato la 2 e l' ho selezionata
+        else if (selectedCard2 == null && card != selectedCard1 && selected)
+        {
+            selectedCard2 = card;
+
+            if (selectedCard1.Compare(selectedCard2))
+            {
+                // ok match!
+                matchedPairAudioSource.Play();
+                selectedCard1.HideAndDestroy();
+                selectedCard2.HideAndDestroy();
+                selectedCard1 = null;
+                selectedCard2 = null;
+                pairs--;
+
+                if (pairs == 0)
+                {
+                    // GameOver
+                    winUI.SetActive(true);
+                    winUI.GetComponent<AudioSource>().Play();
+                }
+            }
+            else
+            {
+                //flip back
+                selectedCard1.ResetMe(); // flip back
+                selectedCard2.ResetMe();
+                
+                //play error sound
+                selectedCard1 = null;
+                selectedCard2 = null;
+            }
+        }
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
---
+++ Assets/Scripts/InteractiveCard.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class InteractiveCard : MonoBehaviour
+{
+    
+    bool selected;
+    float fadeSpeed = 1;
+    bool rotating = false;
+    [SerializeField] private float _rotatingSpeed = 0.8f;
+    AudioSource audioSource;
+
+    public delegate void ClickAction(InteractiveCard card, bool selected);
+    public event ClickAction OnClicked;
+
+    private string _imageName;
+
+    public string imageName { get => _imageName; set => _imageName = value; }
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, 180, 0); // start flipped
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnMouseUp()
+    {
+        // swap card
+        if (rotating) return;
+        rotating = true;
+        selected = !selected;
+        StartCoroutine(RotateMe(Vector3.zero, _rotatingSpeed , selected));
+    }
+
+    public void ResetMe()
+    {
+        selected = false;
+        StartCoroutine(RotateMe(Vector3.up * -180, _rotatingSpeed , selected));
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    IEnumerator RotateMe(Vector3 byAngles, float inTime, bool isSelected)
+    {
+        audioSource.Play();
+
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(byAngles);
+
+        for (var t = 0f; t <= 1; t += Time.deltaTime / inTime)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t); // ANIMA LA ROTAZIONE
+            yield return null;
+        }
+
+        OnClicked(this, isSelected); // notify Manager card was selected
+        rotating = false;
+    }
+
+    public bool Compare(InteractiveCard other)
+    {
+        return imageName == other.imageName; // VERIFICA DUE CARD SIANO UGUALI CONFRONTANDOLE IN BASE AL NOME DEL FILE
+    }
+
+    internal void HideAndDestroy()
+    {
+        // animation fade, or script fade or shader fade...
+        var material = GetComponent<Renderer>().material;
+        StartCoroutine(FadeAndHideCoroutine(material));
+    }
+
+    IEnumerator FadeAndHideCoroutine(Material mat)
+    {
+        while (mat.GetFloat("_Alpha") < 1) // ANIMA LA TRANSIZIONE A TRASPARENTE
+        {
+            var newAlpha = Mathf.MoveTowards(mat.GetFloat("_Alpha"), 1, fadeSpeed * Time.deltaTime);
+            mat.SetFloat("_Alpha", newAlpha);
+            yield return null;
+        }
+        Destroy(gameObject); // AL TERMINE DELL'ANIMAZIONE DISTRUGGE LA CARD
+    }
+}
